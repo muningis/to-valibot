123c71087e01831d2d10d0c9550b050bb7cac5fd
Rokas Muningis <28229273+muningis@users.noreply.github.com>
2025-04-15 08:28:10 +0300

    style: autofix

--- lib/index.ts
+++ lib/index.ts
@@ -12,7 +12,10 @@ type GenerateOptions =
   | { format: 'openapi-json' | 'json'; schemas: Record<string, object> }
   | { format: 'openapi-json' | 'json' | 'openapi-yaml'; schema: string }
   | { format: 'openapi-json' | 'json' | 'openapi-yaml'; schemas: string[] }
-  | { format: 'openapi-json' | 'json' | 'openapi-yaml'; schemas: Record<string, object> };
+  | {
+      format: 'openapi-json' | 'json' | 'openapi-yaml';
+      schemas: Record<string, object>;
+    };
 
 interface ValibotGeneratorReturn {
   generate: (opt: GenerateOptions) => Promise<void>;
@@ -38,18 +41,17 @@ const valibotGenerator = (
       }
     } else if ('schemas' in opt && !Array.isArray(opt.schemas)) {
       for (const [key, schema] of Object.entries(opt.schemas)) {
-        const schemaCode =
-        new ValibotGenerator(
+        const schemaCode = new ValibotGenerator(
           schema,
           opt.format as 'openapi-json' | 'json'
-        )
+        );
 
         const code = schemaCode.generate();
         const name = slugify(key);
         console.log(code, name, `${options.outDir}/${name}.ts`);
         await writeFile(`${options.outDir}/${name}.ts`, code);
       }
-    } else if ("schema" in opt) {
+    } else if ('schema' in opt) {
       const schemaCode =
         typeof opt.schema === 'string'
           ? new ValibotGenerator(opt.schema, opt.format)
